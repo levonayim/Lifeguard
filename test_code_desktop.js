059bcb36a6a9ee00f7c5906f4e15f7b2470ff483
CityOfSurrey-Lifeguard <P206073@WS10314.surrey.ca>
2016-02-05 16:03:45 -0800

    updated Red Cross

--- test_code_desktop.js
+++ test_code_desktop.js
@@ -259,10 +259,12 @@ lifeguard.desktop = {
 				$('#red_cross_swimming_instructor_div_buttons',this.target).css( "display", "none" );
 				$('#red_cross_swimming_instructor_div_notes',this.target).css( "visibility", "hidden" );
 			} else if (ageIndex == 1){
-				$('#red_cross_swimming_instructor_div_buttons',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor_div_buttons .emergency_first_aid',this.target).css( "visibility", "hidden" );
+				$('#red_cross_swimming_instructor_div_buttons .water_safety_instructor',this.target).css( "visibility", "hidden" );
 				$('#red_cross_swimming_instructor_div_notes',this.target).css( "visibility", "hidden" );
+
 			} else if (ageIndex == 2){
-				$('#red_cross_swimming_instructor_div_buttons',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor_div_buttons .water_safety_instructor',this.target).css( "visibility", "hidden" );
 				$('#red_cross_swimming_instructor_div_notes',this.target).css( "visibility", "hidden" );
 			} else if (ageIndex == 3){
 				$('#red_cross_swimming_instructor_div_sorry',this.target).css( "display", "none" );
