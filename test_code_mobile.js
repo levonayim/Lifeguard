d9c095f931685d3bcb4a72c72a07d945ea7d8bd0
CityOfSurrey-Lifeguard <P206073@surrey.ca>
2016-03-15 13:12:51 -0700

    added Red Cross Mobile

--- test_code_mobile.js
+++ test_code_mobile.js
@@ -220,6 +220,7 @@ lifeguard.mobile = {
 				$('#red_cross_swimming_instructor .water_safety_instructor1',this.target).addClass("ui-state-disabled");
 				$('#red_cross_swimming_instructor .water_safety_instructor2',this.target).addClass("ui-state-disabled");
 				$('#red_cross_swimming_instructor .water_safety_instructor_recertification',this.target).addClass("ui-state-disabled");
+
 			} else if (ageIndex == 1){
 				$('#red_cross_swimming_instructor .emergency_first_aid',this.target).addClass("ui-state-disabled");
 				$('#red_cross_swimming_instructor .standard_first_aid',this.target).addClass("ui-state-disabled");
@@ -230,12 +231,22 @@ lifeguard.mobile = {
 				$('#red_cross_swimming_instructor #available_after_age_13',this.target).css( "display", "none" );
 
 			} else if (ageIndex == 2){
-				$('#red_cross_swimming_instructor_div_sorry',this.target).css( "display", "none" );
-				$('#red_cross_swimming_instructor_div_buttons .water_safety_instructor',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor .water_safety_instructor1',this.target).addClass("ui-state-disabled");
+				$('#red_cross_swimming_instructor .water_safety_instructor2',this.target).addClass("ui-state-disabled");
+				$('#red_cross_swimming_instructor .water_safety_instructor_recertification',this.target).addClass("ui-state-disabled");
+
+				$('#red_cross_swimming_instructor #available_after_age_13',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_14',this.target).css( "display", "none" );
+
 			} else if (ageIndex == 3){
-				$('#red_cross_swimming_instructor_div_sorry',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_13',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_14',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_15',this.target).css( "display", "none" );
+				\
 			} else if (ageIndex == 4){
-				$('#red_cross_swimming_instructor_div_sorry',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_13',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_14',this.target).css( "display", "none" );
+				$('#red_cross_swimming_instructor #available_after_age_15',this.target).css( "display", "none" );
 			}
 	},
 	processResult_bcrpa_aquafit_instructor: function(ageIndex){
